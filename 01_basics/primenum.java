90a7872d19e8a3a3f1fda7201c2194ae6a6fd0fc
Riddhesh <135740066+404reese@users.noreply.github.com>
2026-06-08 22:20:46 +0530

    primenum

---
+++ 01_basics/primenum.java
@@ -0,0 +1,20 @@
+// 1 to n all prime number 
+
+class Main {
+    public static void main(String[] args) {
+         int n = 100;
+        for(int i=2;i<=n;i++) {
+            boolean isPrime=true;
+            for (int j=2;j<=i/2;j++){
+                if (i%j==0) {
+                    isPrime=false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println(i);
+            }
+            
+        }
+    }
+}
